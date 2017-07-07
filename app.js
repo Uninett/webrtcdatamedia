0ad8f0ff9251908da6baa2e29dce93bfa16be973
hungqt <hqthieu@gmail.com>
2017-07-07 08:51:11 +0200

    Moved all io stuff inside pem.createCertificate. SSL certificates work now

--- app.js
+++ app.js
@@ -2,10 +2,11 @@
 
 var express = require('express');
 var https = require('https');
-var io = require('socket.io').listen(https);
 var pem = require('pem');
 var path = require("path");
 var connections = [];
+var serviceKey;
+var certificate;
 
 pem.createCertificate({days:1, selfSigned:true}, function(err, keys) {
   var app = express();
@@ -20,54 +21,58 @@ pem.createCertificate({days:1, selfSigned:true}, function(err, keys) {
   app.use('/styles', express.static(path.join(__dirname, '/styles')));
 
   var server = https.createServer({key: keys.serviceKey, cert: keys.certificate}, app).listen(8080);
+  var io = require('socket.io').listen(server);
   console.log('Server running at port ' + '8080');
-});
 
-io.sockets.on('connection', function(socket) {
-  connections.push(socket);
-  console.log('Connected: %s sockets connected', connections.length);
+  io.sockets.on('connection', function(socket) {
+    connections.push(socket);
+    console.log('Connected: %s sockets connected', connections.length);
 
-  // Convenience function to log server messages on the client (client listens to it on socket.on('log'))
-  function log() {
-    var array = ['Message from server: '];
-    array.push.apply(array, arguments);
-    socket.emit('log', array);
-  }
-
-  socket.on('message', function(message) {
-    log('Client said: ', message);
-    // for a real app, would be room-only (not broadcast)
-    socket.broadcast.emit('message', message);
-  });
-
-  socket.on('disconnect', function(data) {
-    connections.splice(connections.indexOf(socket), 1);
-    console.log('Disconnected: %s sockets connected', connections.length);
-  });
-
-  socket.on('create or join', function(room) {
-    // Total number of clients in the socket
-    var numClients = io.engine.clientsCount;
-    log('Received request to create or join room ' + room);
-
-    if(numClients === 1) {
-      socket.join(room);
-      log('Client ID ' + socket.id + ' created room ' + room);
-      socket.emit('created', room, socket.id);
-
-    } else if(numClients === 2) {
-      log('Client ID ' + socket.id + ' joined room ' + room);
-      socket.join(room);
-      socket.emit('joined', room, socket.id);
-      io.sockets.in(room).emit('ready');
-
-    } else {
-      // Max two clients for now
-      socket.emit('full', room);
+    // Convenience function to log server messages on the client (client listens to it on socket.on('log'))
+    function log() {
+      var array = ['Message from server: '];
+      array.push.apply(array, arguments);
+      socket.emit('log', array);
     }
 
+    socket.on('message', function(message) {
+      log('Client said: ', message);
+      // for a real app, would be room-only (not broadcast)
+      socket.broadcast.emit('message', message);
+    });
+
+    socket.on('disconnect', function(data) {
+      connections.splice(connections.indexOf(socket), 1);
+      console.log('Disconnected: %s sockets connected', connections.length);
+    });
+
+    socket.on('create or join', function(room) {
+      // Total number of clients in the socket
+      var numClients = io.engine.clientsCount;
+      log('Received request to create or join room ' + room);
+
+      if(numClients === 1) {
+        socket.join(room);
+        log('Client ID ' + socket.id + ' created room ' + room);
+        socket.emit('created', room, socket.id);
+
+      } else if(numClients === 2) {
+        log('Client ID ' + socket.id + ' joined room ' + room);
+        socket.join(room);
+        socket.emit('joined', room, socket.id);
+        io.sockets.in(room).emit('ready');
+
+      } else {
+        // Max two clients for now
+        socket.emit('full', room);
+      }
+
+    });
   });
+
 });
+
+
 
 /* Function to find out how many clients there are in a room
    Used to minimize each room to contain x clients */
