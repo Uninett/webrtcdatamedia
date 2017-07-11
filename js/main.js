0589569a011147bb6151b4cbd008371fe61ed27f
hungqt <hqthieu@gmail.com>
2017-07-11 15:17:49 +0200

    Added new stun server for test

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 'use strict';
 var configuration = {
   'iceServers': [{
-    'urls': 'stun:stun.l.google.com:19302', 'stun1.l.google.com:19302', 'stunserver.org'
+    'urls': 'stunserver.org'
   }]
 };
 
