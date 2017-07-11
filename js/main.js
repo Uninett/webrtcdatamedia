b81f32a9526b2e3434b7bc0697c37099ba8a2988
hungqt <hqthieu@gmail.com>
2017-07-11 15:08:08 +0200

    Added new stun

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 'use strict';
 var configuration = {
   'iceServers': [{
-    'urls': 'stun:stun.l.google.com:19302'
+    // 'urls': 'stun:stun.l.google.com:19302'
+    'urls': 'stun:webrtc-stud.uninett.no'
   }]
 };
 
